e471baf91940beefbd1b74eeda410a18079bb53a
Bae <qpyu66@naver.com>
2021-04-15 17:04:29 +0900

    자동화기능 json 데이터 전송

--- src/main/java/com/ankus/openapi/controller/OpenapiController.java
+++ src/main/java/com/ankus/openapi/controller/OpenapiController.java
@@ -1,7 +1,11 @@
 package com.ankus.openapi.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 public class OpenapiController {
@@ -15,8 +19,39 @@ public class OpenapiController {
 
 
 
+    @RequestMapping(value="/openparam1",method =  RequestMethod.POST)
+    @ResponseBody
+    public Map<String,String> openParam1(HttpServletRequest request, @RequestParam LinkedHashMap params) throws Exception {
+        System.out.println("params > " + params + ", request > " + request);
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>(params);
+
+        String kv = "";
+        //json 으로 받은 key, value 출력
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println("map key > " + key + " , value > " + value);
+            if (key == "addrs" || key == "servicekey") {
+                kv = kv;
+            } else {
+                kv = kv + "&" + key + "=" + value;
+            }
+
+        }
+
+        System.out.println("map sizee > " + map.size() + ", kv > " + kv);
+
+        String addrs = request.getParameter("addrs");
+        String sk = request.getParameter("servicekey");
+        System.out.println("map sizee > " + map.size() + " ,addr > " + addrs + " ,sk > " + sk + " , kv > " + kv);
+
+
+
+        return params;
+    }
 
 
 
 
-}
+
+
+    }
